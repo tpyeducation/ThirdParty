f07e9ffe7f0feb198574eb61439cb4336b53e205
Murali <tariniply@gmail.com>
2025-05-22 18:33:03 +0100

    wwwwew

---
+++ src/main/java/uk/family/ecommerce/person/Tarini.java
@@ -0,0 +1,32 @@
+package uk.family.ecommerce.person;
+
+public class Tarini {
+
+    public int add(int a, int b, int c, int d) {
+        int x = a + b + c + d;
+        return x;
+    }
+
+    public int multiply(int a, int b) {
+        int x = a * b;
+        return x;
+    }
+
+    public int divide(int a, int b) {
+        int x = a / b;
+        return x;
+    }
+
+    public int minus(int a, int b) {
+        int x = a - b;
+        return x;
+    }
+
+    public int reminder (int i, int j) {
+      int m = i  % j;
+      return m;
+
+
+    }
+
+}
